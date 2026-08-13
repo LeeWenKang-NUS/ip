d030213efd8e8453e947287d09c3621d81b6b7f4
Your Name <you@example.com>
2026-08-13 16:19:14 +0800

    Rename chatbot class

--- src/main/java/Auto.java
+++ src/main/java/Auto.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Auto {
     public static void main(String[] args) {
         String banner = "    _         _        \n"
                 + "   / \\  _   _| |_ ___  \n"
