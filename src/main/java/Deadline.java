73277d41af0355c50b5c8862c2fb02c53fbf062d
Lee Wen Kang <leewenkang03@gmail.com>
2026-08-13 22:18:46 +0800

    Add Deadline, Event and ToDo classes

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,12 @@
+public class Deadline extends Task {
+    private String deadline;
+
+    public Deadline(String name, String deadline) {
+        super(name);
+        this.deadline = deadline;
+    }
+
+    public String toString() {
+        return String.format("[D]%s", super.toString());
+    }
+}
