01854f7e9356834810c13a0d11e3e964d146e78d
Lee Wen Kang <leewenkang03@gmail.com>
2026-08-14 16:17:48 +0800

    Improved task parsing and error handling

--- src/main/java/Auto.java
+++ src/main/java/Auto.java
@@ -21,73 +21,100 @@ public class Auto {
         while (true) {
             String userInput = scanner.nextLine();
 
-            if (userInput.equals("bye")) {
-                System.out.println("=======================================================");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("=======================================================");
-                break;
-            } else if (userInput.equals("list")) {
-                System.out.println("=======================================================");
-                for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println(String.format("%d. %s", i + 1, taskList.get(i)));
+            try {
+                if (userInput.equals("bye")) {
+                    System.out.println("=======================================================");
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("=======================================================");
+                    break;
+                } else if (userInput.equals("list")) {
+                    System.out.println("=======================================================");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        System.out.println(String.format("%d. %s", i + 1, taskList.get(i)));
+                    }
+                    System.out.println("=======================================================");
+                } else if (userInput.startsWith("mark ")) {
+                    int taskIndex = parseTaskIndex(userInput.substring(5), taskList.size());
+                    Task task = taskList.get(taskIndex);
+                    task.mark();
+                    System.out.println("=======================================================");
+                    System.out.println("Nice! I've marked this task as done");
+                    System.out.println("  " + task);
+                    System.out.println("=======================================================");
+                } else if (userInput.startsWith("unmark ")) {
+                    int taskIndex = parseTaskIndex(userInput.substring(7), taskList.size());
+                    Task task = taskList.get(taskIndex);
+                    task.unmark();
+                    System.out.println("=======================================================");
+                    System.out.println("Nice! I've marked this task as not done yet");
+                    System.out.println("  " + task);
+                    System.out.println("=======================================================");
+                } else if (userInput.startsWith("todo ")) {
+                    String taskName = userInput.substring(5);
+                    Task newTask = new ToDo(taskName);
+                    taskList.add(newTask);
+                    System.out.println("=======================================================");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + newTask);
+                    System.out.println(String.format("Now you have %d tasks in the list.", taskList.size()));
+                    System.out.println("=======================================================");
+                } else if (userInput.startsWith("deadline ")) {
+                    String[] parts = userInput.substring(9).split(" /by ", 2);
+                    if (parts.length < 2) {
+                        throw AutoException.deadlineNeedsBy();
+                    }
+                    String taskName = parts[0].trim();
+                    String by = parts[1].trim();
+                    Task newTask = new Deadline(taskName, by);
+                    taskList.add(newTask);
+                    System.out.println("=======================================================");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + newTask);
+                    System.out.println(String.format("Now you have %d tasks in the list.", taskList.size()));
+                    System.out.println("=======================================================");
+                } else if (userInput.startsWith("event ")) {
+                    String[] nameAndTime = userInput.substring(6).split(" /from ", 2);
+                    if (nameAndTime.length < 2) {
+                        throw AutoException.eventNeedsFromAndTo();
+                    }
+                    String[] fromAndTo = nameAndTime[1].split(" /to ", 2);
+                    if (fromAndTo.length < 2) {
+                        throw AutoException.eventNeedsFromAndTo();
+                    }
+                    String taskName = nameAndTime[0].trim();
+                    String from = fromAndTo[0].trim();
+                    String to = fromAndTo[1].trim();
+                    Task newTask = new Event(taskName, from, to);
+                    taskList.add(newTask);
+                    System.out.println("=======================================================");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + newTask);
+                    System.out.println(String.format("Now you have %d tasks in the list.", taskList.size()));
+                    System.out.println("=======================================================");
+                } else {
+                    throw AutoException.unknownCommand();
                 }
+            } catch (AutoException e) {
                 System.out.println("=======================================================");
-            } else if (userInput.startsWith("mark ")) {
-                int taskIndex = Integer.parseInt(userInput.substring(5).trim()) - 1;
-                Task task = taskList.get(taskIndex);
-                task.mark();
-                System.out.println("=======================================================");
-                System.out.println("Nice! I've marked this task as done");
-                System.out.println("  " + task);
-                System.out.println("=======================================================");
-            } else if (userInput.startsWith("unmark ")) {
-                int taskIndex = Integer.parseInt(userInput.substring(7).trim()) - 1;
-                Task task = taskList.get(taskIndex);
-                task.unmark();
-                System.out.println("=======================================================");
-                System.out.println("Nice! I've marked this task as not done yet");
-                System.out.println("  " + task);
-                System.out.println("=======================================================");
-            } else if (userInput.startsWith("todo ")) {
-                String taskName = userInput.substring(5);
-                Task newTask = new ToDo(taskName);
-                taskList.add(newTask);
-                System.out.println("=======================================================");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + newTask);
-                System.out.println(String.format("Now you have %d tasks in the list.", taskList.size()));
-                System.out.println("=======================================================");
-            } else if (userInput.startsWith("deadline ")) {
-                String[] parts = userInput.substring(9).split(" /by ", 2);
-                String taskName = parts[0].trim();
-                String by = parts[1].trim();
-                Task newTask = new Deadline(taskName, by);
-                taskList.add(newTask);
-                System.out.println("=======================================================");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + newTask);
-                System.out.println(String.format("Now you have %d tasks in the list.", taskList.size()));
-                System.out.println("=======================================================");
-            } else if (userInput.startsWith("event ")) {
-                String[] nameAndTime = userInput.substring(6).split(" /from ", 2);
-                String[] fromAndTo = nameAndTime[1].split(" /to ", 2);
-                String taskName = nameAndTime[0].trim();
-                String from = fromAndTo[0].trim();
-                String to = fromAndTo[1].trim();
-                Task newTask = new Event(taskName, from, to);
-                taskList.add(newTask);
-                System.out.println("=======================================================");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + newTask);
-                System.out.println(String.format("Now you have %d tasks in the list.", taskList.size()));
-                System.out.println("=======================================================");
-            } else {
-                System.out.println("=======================================================");
-                System.out.println("Ohhh Noooo... I don't understand you!");
+                System.out.println(e.getMessage());
                 System.out.println("=======================================================");
             }
         }
 
         scanner.close();
+    }
+
+    private static int parseTaskIndex(String argument, int taskCount) throws AutoException {
+        String trimmed = argument.trim();
+        int taskNumber;
+        try {
+            taskNumber = Integer.parseInt(trimmed);
+        } catch (NumberFormatException e) {
+            throw AutoException.notATaskNumber(trimmed);
+        }
+        if (taskNumber < 1 || taskNumber > taskCount) {
+            throw AutoException.noSuchTask(taskNumber);
+        }
+        return taskNumber - 1;
     }
 }
---
+++ src/main/java/AutoException.java
@@ -0,0 +1,27 @@
+public class AutoException extends Exception {
+    private static final String PREFIX = "Ohhh Noooo... ";
+
+    private AutoException(String message) {
+        super(PREFIX + message);
+    }
+
+    public static AutoException unknownCommand() {
+        return new AutoException("I don't understand you!");
+    }
+
+    public static AutoException noSuchTask(int taskNumber) {
+        return new AutoException(String.format("there is no task %d!", taskNumber));
+    }
+
+    public static AutoException notATaskNumber(String argument) {
+        return new AutoException(String.format("'%s' is not a task number!", argument));
+    }
+
+    public static AutoException deadlineNeedsBy() {
+        return new AutoException("a deadline needs a /by date!");
+    }
+
+    public static AutoException eventNeedsFromAndTo() {
+        return new AutoException("an event needs a /from and a /to time!");
+    }
+}
